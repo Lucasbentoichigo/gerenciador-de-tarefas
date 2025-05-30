e342d6333f8e65eefc642d6826e329dae9c3e4de
Lucas <lucastavaresbento@gmail.com>
2025-05-30 08:24:23 -0300

    Adiciona novas tarefas

--- tarefas.js
+++ tarefas.js
@@ -1,1 +1,17 @@
-let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+let tarefas = ['Estudar JavaScript',
+     'Fazer exercícios de matemática',
+      'Lavar a louça', 
+      'Comprar mercado',
+       'Ir à academia',
+        'Ler um livro',
+         'Fazer revisão para o teste',
+          'Limpar o quarto',
+           'Estudar para concurso',
+            'Organizar o computador'];
+
+console.log(`Antes do push:${tarefas}`);
+
+tarefas.push("Cozinhar");
+tarefas.push("Ir a igreja");
+
+console.log(`Depois do push: ${tarefas}`);
