d70d2c4f5bead00c61815446fc4ae522fe3c2dfe
Lucas <lucastavaresbento@gmail.com>
2025-05-30 08:43:22 -0300

    Remove primeira tarefa

--- tarefas.js
+++ tarefas.js
@@ -9,6 +9,7 @@ let tarefas = ['Estudar JavaScript',
            'Estudar para concurso',
             'Organizar o computador'];
 
+            //Adicione ao menos 2 novas tarefas à lista de tarefas.
 console.log(`Antes do push:${tarefas}`);
 
 tarefas.push("Cozinhar");
@@ -17,12 +18,18 @@ tarefas.push("Ir a igreja");
 console.log(`Depois do push: ${tarefas}`);
 console.log("------------------------------------------------------------------------------------------------------------------------------------------------------------");
 
+//Remova a última tarefa da lista.
 let tarefaRemovida = tarefas.pop()
 console.log(`Depois do pop:${tarefas}`);
 console.log(`Tarefa removida:${tarefaRemovida}`);
 console.log("------------------------------------------------------------------------------------------------------------------------------------------------------------");
 
+//Adicione uma tarefa no início da lista.
 tarefas.unshift("Assistir filme");
-
 console.log(`Depois do unshifted:${tarefas}`);
 console.log("------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//Remova a primeira tarefa da lista.
+let tarefaRemovida2 = tarefas.shift();
+console.log(`Depois do shift:${tarefas}`);
+console.log(`tarefa removida:${tarefaRemovida2}`);
